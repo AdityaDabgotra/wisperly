f361df27f1137b38d9dcc6629401c4fad8798dfa
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-02-08 17:24:03 +0530

    sign in page completed

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,11 +1,114 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/schemas/signInSchema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import Link from "next/link";
 
 const page = () => {
-  return (
-    <div>
-      Sign In Page
-    </div>
-  )
-}
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-export default page
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+
+    const res = await signIn("credentials", {
+      identifier: data.identifier,
+      password: data.password,
+      redirect: false,
+    });
+
+    setIsSubmitting(false);
+
+    if (res?.error) {
+      toast.error(res.error);
+    }
+    if(res?.url){
+      router.replace('/dashboard');
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Login Wisperly
+          </h1>
+          <p className="mb-4">Sign In to start your Anonymous Adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your Username or Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={isSubmitting} type="submit">
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="animate-spin mr-2 h-4 w-4" />
+                  Signing In...
+                </>
+              ) : (
+                "SignIn"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Don't have account?{" "}
+            <Link href="/signUp" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,72 +4,77 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/db";
 import UserModel from "@/model/User";
 
-
 export const authOptions: NextAuthOptions = {
-    providers:[
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "Enter your Username" },
-                password: { label: "Password", type: "password" }
-            },
-
-            async authorize(credentials: any):Promise<any> {
-                await dbConnect();
-                try {
-                    const user = await UserModel.findOne({
-                        $or:[
-                            {email: credentials.identifier.email},
-                            {username: credentials.identifier.username}
-                        ]
-                    });
-                    if(!user){
-                        throw new Error("No user found with the given credentials");
-                    }
-                    if(!user.isVerified){
-                        throw new Error("Please verify your email to login");
-                    }
-                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-                    if(isPasswordCorrect){
-                        return user;
-                    }
-                    else{
-                        throw new Error("Incorrect password");
-                    }
-                } catch (error:any) {
-                    throw new Error(error.message);
-                }
-            }
-        })
-    ],
-    callbacks:{
-        async jwt({ token, user }) {
-            if(user) {
-                token._id = user._id?.toString();
-                token.isVerified = user.isVerified;
-                token.username = user.username;
-                token.isAcceptingMessages = user.isAcceptingMessages; 
-            }
-
-            return token
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        identifier: {
+          label: "Email or Username",
+          type: "text",
+          placeholder: "Enter your email or username",
         },
-        async session({ session, token }) {
-            if(token){
-                session.user._id = token._id?.toString();
-                session.user.isVerified = token.isVerified;
-                session.user.username = token.username;
-                session.user.isAcceptingMessages = token.isAcceptingMessages;
-            }
-            return session
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        const user = await UserModel.findOne({
+          $or: [
+            { email: credentials.identifier },
+            { userName: credentials.identifier },
+          ],
+        });
+
+        if (!user) {
+          throw new Error("No user found with the given credentials");
         }
+
+        if (!user.isVerified) {
+          throw new Error("Please verify your email to login");
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordCorrect) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.username = user.username;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+      }
+
+      return token;
     },
-    pages: {
-        signIn: "/signIn",
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id?.toString();
+        session.user.isVerified = token.isVerified;
+        session.user.username = token.username;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+      }
+      return session;
     },
-    session:{
-        strategy: "jwt"
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    
-}
+  },
+  pages: {
+    signIn: "/signIn",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
