a6e26387ab6e40e3d4b43c21070e6b21cf7fd3bf
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-02-09 19:08:04 +0530

    delete message route completed

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,49 @@
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/db";
+import UserModel from "@/model/User";
+import mongoose from "mongoose";
+
+export async function DELETE(request: Request,{params}: {params:{messageid:string}}) {
+    const messageId = params.messageid;
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    
+    const user: User = session?.user as User;
+    const userId = new mongoose.Types.ObjectId(session?.user._id);
+
+    if (!session || !session.user || !userId) {
+        return Response.json({ 
+            success: false,
+            message: "Not Authenticated" 
+        }),
+        { status: 401 };
+    }
+
+    try {
+        const updatedResult = await UserModel.updateOne(
+            {_id: user._id},
+            {$pull:{messages: {_id: messageId}}}
+        )
+        if(updatedResult.modifiedCount == 0){
+            return Response.json({ 
+                success: false,
+                message: " Message not found or already deleted" 
+            }),
+            { status: 404 };
+        }
+        return Response.json({ 
+            success: true,
+            message: "Message deleted successfully" 
+        }),
+        { status: 200 };
+    } catch (error) {
+        console.log("Error deleting message:", error);
+        
+        return Response.json({
+            success: false,
+            message: "Error deleting message",
+        }),
+        { status: 500 };
+    }
+}
